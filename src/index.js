4f65d2996f8a55c36bb504f83ec4fcb2c04934dd
Sergey Neplashov <znichu1989@gmail.com>
2020-04-15 10:45:51 +0300

    Added store and methods for store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
-import state, {subscribe} from "./redux/state";
+import store from "./redux/state";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
-import {addMessage, addPost, updateMessageText, updatePostText} from "./redux/state";
 import {BrowserRouter} from "react-router-dom";
 
 
@@ -14,15 +13,20 @@ let rerenderTree = (state) => {
 
     ReactDOM.render(
         < BrowserRouter>
-            <App state={state} addPost={addPost} updatePostText={updatePostText}
-                 addMessage={addMessage} updateMessageText={updateMessageText}/>
+            <App
+                state={state}
+                addPost={store.addPost.bind(store)}
+                updatePostText={store.updatePostText.bind(store)}
+                addMessage={store.addMessage.bind(store)}
+                updateMessageText={store.updateMessageText.bind(store)}
+            />
         </BrowserRouter>, document.getElementById('root'));
 
 }
 
-rerenderTree(state);
+rerenderTree(store.getState());
 
-subscribe(rerenderTree);
+store.subscribe(rerenderTree);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,92 +1,90 @@
-let rerenderTree = () => {
-    console.log();
-}
-
-let state = {
-    myPostsPage: {
-        posts: [
-            {
-                id: 1,
-                message: 'Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for ',
-                likesCount: 25
-            },
-            {
-                id: 2,
-                message: 'Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for Many desktop publishing duskam azer. ',
-                likesCount: 32
-            },
-            {
-                id: 3,
-                message: 'Images come in all sizes. So do screens. Responsive images automatically adjust to fit the size of the screen.',
-                likesCount: 105
-            }
-        ],
-        newPostText:''
+let store = {
+    _state: {
+        myPostsPage: {
+            posts: [
+                {
+                    id: 1,
+                    message: 'Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for ',
+                    likesCount: 25
+                },
+                {
+                    id: 2,
+                    message: 'Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for Many desktop publishing duskam azer. ',
+                    likesCount: 32
+                },
+                {
+                    id: 3,
+                    message: 'Images come in all sizes. So do screens. Responsive images automatically adjust to fit the size of the screen.',
+                    likesCount: 105
+                }
+            ],
+            newPostText:''
+        },
+        messagesPage: {
+            dialogs: [
+                {id: 1, name: 'Dimych'},
+                {id: 2, name: 'Serg'},
+                {id: 3, name: 'Juliya'},
+                {id: 4, name: 'Sasha'},
+                {id: 5, name: 'Lena'}
+            ],
+            messages: [
+                {id: 1, message: 'Hello.'},
+                {id: 2, message: 'How are you?'},
+                {id: 3, message: 'I\'m fine.'},
+                {id: 4, message: 'What you did now?'},
+                {id: 5, message: 'Read book.'},
+                {id: 6, message: 'Cool.'},
+            ],
+            newMessageText: ''
+        },
+        friendsBlock:{
+            friend: [
+                {id: 1, name: 'Serg'},
+                {id: 2, name: 'Dima'},
+                {id: 3, name: 'Lena'}
+            ]
+        }
     },
-    messagesPage: {
-        dialogs: [
-            {id: 1, name: 'Dimych'},
-            {id: 2, name: 'Serg'},
-            {id: 3, name: 'Juliya'},
-            {id: 4, name: 'Sasha'},
-            {id: 5, name: 'Lena'}
-        ],
-        messages: [
-            {id: 1, message: 'Hello.'},
-            {id: 2, message: 'How are you?'},
-            {id: 3, message: 'I\'m fine.'},
-            {id: 4, message: 'What you did now?'},
-            {id: 5, message: 'Read book.'},
-            {id: 6, message: 'Cool.'},
-        ],
-        newMessageText: ''
+    getState() {
+      return this._state;
     },
-    friendsBlock:{
-        friend: [
-            {id: 1, name: 'Serg'},
-            {id: 2, name: 'Dima'},
-            {id: 3, name: 'Lena'}
-        ]
-    }
-}
+    _callSubscriber () {
+        console.log('State changed');
+    },
+    addPost () {   //функция для добавления нвого поста в стэйт
+        let newPost = {
+            id: 4,
+            message: this._state.myPostsPage.newPostText,
+            likesCount: 0
+        };
+        this._state.myPostsPage.posts.push(newPost); //добавляет новый пост в state для добавления используеться метод push()
+        this._state.myPostsPage.newPostText = ''; //обнуление, зачищает поле ввода поста
+        this._callSubscriber(this._state);
+    },
+    updatePostText (newText) {
+        this._state.myPostsPage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    addMessage () {
+        let newMessage = {
+            id: 7,
+            message: this._state.messagesPage.newMessageText
+        };
+        this._state.messagesPage.messages.push(newMessage);
+        this._state.messagesPage.newMessageText = '';
+        this._callSubscriber(this._state);
+    },
+    updateMessageText (newTextMessage) {
+        this._state.messagesPage.newMessageText = newTextMessage;
+        this._callSubscriber(this._state);
+    },
+    subscribe (observer) {
+        this._callSubscriber = observer
+    },
+};
 
-window.state = state;
-
-export const addPost = () => {   //функция для добавления нвого поста в стэйт
-    let newPost = {
-        id: 4,
-        message: state.myPostsPage.newPostText,
-        likesCount: 0
-    };
-    state.myPostsPage.posts.push(newPost); //добавляет новый пост в state для добавления используеться метод push()
-    state.myPostsPage.newPostText = ''; //обнуление, зачищает поле ввода поста
-    rerenderTree(state);
-}
-
-export const updatePostText = (newText) => {
-    state.myPostsPage.newPostText = newText;
-    rerenderTree(state);
-}
-
-export const addMessage = () => {
-    let newMessage = {
-        id: 7,
-        message: state.messagesPage.newMessageText
-    };
-    state.messagesPage.messages.push(newMessage);
-    state.messagesPage.newMessageText = '';
-    rerenderTree(state);
-}
-
-export const updateMessageText = (newTextMessage) => {
-    state.messagesPage.newMessageText = newTextMessage;
-    rerenderTree(state);
-}
-
-export const subscribe = (observer) => {  
-    rerenderTree = observer
-}
-
-export default state;
+window.state = store;
+export default store;
 
 
