a7f2bae88f60f609db1db805dbc0ef39de108234
Znichu <znichu1989@gmail.com>
2020-05-10 23:54:58 +0300

    add class component

--- src/componets/Users/Users.jsx
+++ src/componets/Users/Users.jsx
@@ -3,44 +3,38 @@ import style from './Users.module.css'
 import * as axios from "axios";
 import smallAvatar from './../../assets/images/avatar-chase.png'
 
+class Users extends React.Component {
+    componentDidMount() {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {const users = response.data.items; this.props.setUsers(users)});
+    }
 
-const Users = (props) => {
-    let getUsers = () => {
-        if (props.users.length === 0) {
-            axios.get("https://social-network.samuraijs.com/api/1.0/users")
-                .then(response => {
-                    const users = response.data.items;
-                    props.setUsers(users)
-                })
-        }
-    };
-
-
-    let user = props.users.map( u =>
-        <div className={style.mainContainer}>
-            <div className={`${style.userCard} ${style.clearfix}`}>
-                <img src={u.photos.small != null ? u.photos.small : smallAvatar } className={style.userImg}/>
+    render() {
+        let user = this.props.users.map( u =>
+            <div className={style.mainContainer}>
+                <div className={`${style.userCard} ${style.clearfix}`}>
+                    <img src={u.photos.small != null ? u.photos.small : smallAvatar } className={style.userImg}/>
                     <div className={`${style.col2} ${style.clearfix}`}>
                         <div className={style.fullname}>{u.name}</div>
                         <div className={style.status}>{u.status}</div>
                         <div>City: <span>{"u.location.city"}, {"u.location.country"}</span></div>
                     </div>
-            </div>
-            {u.followed
-            ? <button className={style.nextUser} onClick={() => {props.unsubscribe(u.id)}}>Unfollow</button>
-            : <button className={style.nextUser} onClick={() => {props.follow(u.id)}}>Follow</button>
-            }
-        </div>);
+                </div>
+                {u.followed
+                    ? <button className={style.nextUser} onClick={() => {this.props.unsubscribe(u.id)}}>Unfollow</button>
+                    : <button className={style.nextUser} onClick={() => {this.props.follow(u.id)}}>Follow</button>
+                }
+            </div>);
 
-    return (
-        <div className="container">
-            <div className="row">
-                <button onClick={getUsers}>Get Users</button>
-                {user}
+        return (
+            <div className="container">
+                <div className="row">
+                    {user}
+                </div>
             </div>
-        </div>
-
-    );
+        );
+    }
 }
+
 
 export default Users;
