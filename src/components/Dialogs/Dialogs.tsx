8953463d582fc5601d728ba67bb635360d7375dc
Znichu <sergeyznich@gmail.com>
2020-08-26 22:41:44 +0300

    refactoring Dialogs component

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import s from './Dialogs.module.css'
+import style from './Dialogs.module.css'
 import DialogItem from "./DialogItem/DialogItem";
 import MessageItem from "./MessageItem/MessageItem";
 import {Field, InjectedFormProps, reduxForm} from "redux-form";
 import {Textarea} from "../../common/FormsControls/FormsControls";
 import {required} from "../../utils/Validation/FieldValidationForm";
 import {useDispatch, useSelector} from "react-redux";
+import {actions} from "../../redux/messages-reducer";
 import {RootState} from "../../redux/redux-store";
 
 
@@ -25,26 +26,26 @@ export const Dialogs: React.FC = () => {
     let messagesElement =
         messages.map(m => <MessageItem key={m.id} message={m.message} />);
 
-    let addMessage = (values: AddMessageFormType) => {
-        dispatch(addMessage(values));
+    let addNewMessage = (values: AddMessageFormType) => {
+        dispatch(actions.addMessage(values.addMessageBody));
     };
 
     return (
         <div className="container" style={{paddingBottom: "25px"}}>
             <div className="row">
                 <div className="col-lg-12">
-                    <div className={s.dialogs}>
+                    <div className={style.dialogs}>
                         {dialogsElement}
                     </div>
                 </div>
             </div>
             <div className="row">
                 <div className="col lg-12">
-                    <div className={s.messages}>
+                    <div className={style.messages}>
                         {messagesElement}
                     </div>
-                    <div className={s.enterMessage}>
-                        <AddMessageFormRedux onSubmit={addMessage}/>
+                    <div className={style.enterMessage}>
+                        <AddMessageFormRedux onSubmit={addNewMessage}/>
                     </div>
                 </div>
             </div>
@@ -60,7 +61,7 @@ const AddMessageForm: React.FC<InjectedFormProps<AddMessageFormType>> = (props) 
                 placeholder='Ваше сообщение...'
                 name="addMessageBody"
                 component={Textarea} />
-                <div className={s.send}>
+                <div className={style.send}>
                     <button className="btn btn-primary">Отправить</button>
                 </div>
         </form>
